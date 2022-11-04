dd23eae5441213bb3cd13a621493a98239c8e625
Kosty <doro.kon@mail.ru>
2022-11-04 23:38:23 +0300

    Add write json file in 25-26 practical works

--- src/practice_25_26/ParserJSON.java
+++ src/practice_25_26/ParserJSON.java
@@ -1,11 +1,13 @@
 package practice_25_26;
 
+import com.google.gson.Gson;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,10 +23,10 @@ class Station{
 }
 
 class Line{
-    private int number;
+    private String number;
     private String name;
 
-    public Line(int number, String name) {
+    public Line(String number, String name) {
         this.number = number;
         this.name = name;
     }
@@ -32,6 +34,9 @@ class Line{
 
 public class ParserJSON {
     public static void main(String[] args){
+        String path = "D:\\Programming\\IDEA\\mirea\\src\\practice_25_26\\file.json";
+        String data = "";
+        Gson gson = new Gson();
         ArrayList<Station> stations = new ArrayList<>();
         ArrayList<Line> lines = new ArrayList<>();
         try {
@@ -39,17 +44,35 @@ public class ParserJSON {
             Elements stationsData = document.select("p");
             Elements linesData = document.select("span");
             for (Element el: stationsData){
-                Pattern pattern = Pattern.compile("(\\d+[.]\\s\\w+)");
+                Pattern pattern = Pattern.compile("\\d+[.]\\s[^a-zA-Z]+");
                 Matcher matcher = pattern.matcher(el.text());
                 if (matcher.matches()){
+                    int number = Integer.parseInt(el.text().split(". ")[0]);
+                    String name = el.text().split(". ")[1];
+                    Station station = new Station(number, name);
+                    stations.add(station);
                     System.out.println(el.text());
                 }
             }
             for (Element el: linesData) {
                 if (!el.attr("data-line").isEmpty()) {
-                    System.out.println(el);
+                    Line line = new Line(el.attr("data-line"), el.text());
+                    lines.add(line);
+                    System.out.println(el.text());
                 }
             }
+            ArrayList<Object> arrayList = new ArrayList<>();
+            arrayList.addAll(stations);
+            arrayList.addAll(lines);
+            data = gson.toJson(arrayList);
+//            data += gson.toJson(stations);
+//            data += gson.toJson(lines);
+
+        }catch (Exception e){
+
+        }
+        try(PrintWriter out = new PrintWriter(new FileWriter(path))){
+            out.write(data);
         }catch (Exception e){
 
         }
